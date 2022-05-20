d61ad36401f24b2a7291a2c4674fccc11fe5792f
tangkai <tangkai@miaxis.com>
2022-05-20 20:51:59 +0800

    feat():更新版本号至1.0.22，增加https，更新配置文件，增加签名文件

--- app/src/main/java/com/miaxis/thermal/data/net/BaseAPI.java
+++ app/src/main/java/com/miaxis/thermal/data/net/BaseAPI.java
@@ -1,14 +1,19 @@
 package com.miaxis.thermal.data.net;
 
-import android.text.TextUtils;
-
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import com.miaxis.thermal.data.exception.MyException;
 import com.miaxis.thermal.manager.ConfigManager;
-import com.miaxis.thermal.util.ValueUtil;
 
-import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -19,6 +24,13 @@ public class BaseAPI {
             .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+
+    protected final static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//            .addInterceptor(new LoggerInterceptor("HTTP"))
+            .sslSocketFactory(SSLSocketClient.getSSLSocketFactory())
+            .hostnameVerifier(SSLSocketClient.getHostnameVerifier())
+            //其他配置
+            .build();
 
     protected static Retrofit retrofit;
 
@@ -33,8 +45,51 @@ public class BaseAPI {
     public static void rebuildRetrofit() {
         retrofit = RETROFIT_BUILDER
                 .baseUrl(ConfigManager.getInstance().getConfig().getHost())
+                .client(okHttpClient)
                 .build();
     }
 
+    public static class SSLSocketClient {
+
+        //获取这个SSLSocketFactory
+        public static SSLSocketFactory getSSLSocketFactory() {
+            try {
+                SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, getTrustManager(), new SecureRandom());
+                return sslContext.getSocketFactory();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //获取TrustManager
+        private static TrustManager[] getTrustManager() {
+            return new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[]{};
+                }
+            }};
+        }
+
+        //获取HostnameVerifier
+        public static HostnameVerifier getHostnameVerifier() {
+            return new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            };
+        }
+
+    }
 
 }
